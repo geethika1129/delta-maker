5227174ddf372a5175683ab813ac345bf5b646fb
Geethika <83590629+geethika1129@users.noreply.github.com>
2026-01-17 14:44:55 +0530

    Time: 8 ms (65.1%), Space: 14.8 MB (5.92%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,9 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n=len(nums)
-        c=Counter(nums)
-        for a in nums:
-            if c[a]>n//2:
-                return a
+        # n=len(nums)
+        # c=Counter(nums)
+        # for a in nums:
+        #     if c[a]>n//2:
+        #         return a
+    
+        c=0
+        for i in range(len(nums)):
+            if c==0:
+                k=nums[i]
+                c=1
+            elif nums[i]==k:
+                c+=1
+            else:
+                c-=1
+        return k
+
         
